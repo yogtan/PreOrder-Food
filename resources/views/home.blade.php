6ade27c6ce153d46fd2f39b3c0fa3c51756df747
yogtan <johanesyogtan305@gmail.com>
2023-10-05 19:36:44 +0700

    First GitHub Project Landing Page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,135 @@
+@extends('layouts.main')
+
+@section('container')
+    <section class="hero align-items-center text-center">
+        <div class="container pt-5 text-white">
+            <h1 class="hero-title">Pre-<span class="hero-title2">Order</span></h1>
+            <h3 class="pt-3">Pre-Order Your Fav Food Here!</h3>
+            <p>Order your favorite meals here, on our web. Ass smooth as <br> in the app. Some fast delivery. Countless
+                restos to try.</p>
+        </div>
+    </section>
+
+
+    <section class="product justify-content-center">
+        <div class="container pt-5">
+            <div class="Inpo">
+                <h2 class="product-tittle pt-3"> What's good to eat in USD?</h2>
+                <p>Discover our collection of popular food in
+                    Sanata Dharma</p>
+            </div>
+            <div  class="ProductPertama row pt-5">
+                <div class="col-md-3 ">
+                    <div class="card align-items-center" style="width: 18rem; height: 27rem">
+                        <img src="img/Poster-Product.svg" class="card-img-top" alt="...">
+                        <div style="text-align:left" class="card-body">
+                            <h5 class="card-title">Mas Teknik</h5>
+                            <p class="card-text"><strong>Nasi Goreng Special</strong></p>
+                            <p class="card-text">Rp 15.000</p>
+                            <hr>
+                            <p class="card-text">Pre Order - 1 h</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem; height: 27rem">
+                        <img src="img/Poster-Product.svg" class="card-img-top" alt="...">
+                        <div style="text-align:left" class="card-body">
+                            <h5 class="card-title">Mas Teknik</h5>
+                            <p class="card-text"><strong>Nasi Goreng Special</strong></p>
+                            <p class="card-text">Rp 15.000</p>
+                            <hr>
+                            <p class="card-text">Pre Order - 1 h</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem; height: 27rem">
+                        <img src="img/Poster-Product.svg" class="card-img-top" alt="...">
+                        <div style="text-align:left" class="card-body">
+                            <h5 class="card-title">Mas Teknik</h5>
+                            <p class="card-text"><strong>Nasi Goreng Special</strong></p>
+                            <p class="card-text">Rp 15.000</p>
+                            <hr>
+                            <p class="card-text">Pre Order - 1 h</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem; height: 27rem">
+                        <img src="img/Poster-Product.svg" class="card-img-top" alt="...">
+                        <div style="text-align:left" class="card-body">
+                            <h5 class="card-title">Mas Teknik</h5>
+                            <p class="card-text"><strong>Nasi Goreng Special</strong></p>
+                            <p class="card-text">Rp 15.000</p>
+                            <hr>
+                            <p class="card-text">Pre Order - 1 h</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="ProductKedua row pt-5">
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem; height: 27rem">
+                        <img src="img/Poster-Product2.svg" class="card-img-top" alt="...">
+                        <div style="text-align:left" class="card-body">
+                            <h5 class="card-title">Johanes Yogtannn</h5>
+                            <p class="card-text"><strong>Choco Cokkies</strong></p>
+                            <p class="card-text">Rp 10.000</p>
+                            <hr>
+                            <p class="card-text">Pre Order - 1 h</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem; height: 27rem">
+                        <img src="img/Poster-Product2.svg" class="card-img-top" alt="...">
+                        <div style="text-align:left" class="card-body">
+                            <h5 class="card-title">Johanes Yogtannn</h5>
+                            <p class="card-text"><strong>Choco Cokkies</strong></p>
+                            <p class="card-text">Rp 10.000</p>
+                            <hr>
+                            <p class="card-text">Pre Order - 1 h</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem; height: 27rem">
+                        <img src="img/Poster-Product2.svg" class="card-img-top" alt="...">
+                        <div style="text-align:left" class="card-body">
+                            <h5 class="card-title">Johanes Yogtannn</h5>
+                            <p class="card-text"><strong>Choco Cokkies</strong></p>
+                            <p class="card-text">Rp 10.000</p>
+                            <hr>
+                            <p class="card-text">Pre Order - 1 h</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="card" style="width: 18rem; height: 27rem">
+                        <img src="img/Poster-Product2.svg" class="card-img-top" alt="...">
+                        <div style="text-align:left" class="card-body">
+                            <h5 class="card-title">Johanes Yogtannn</h5>
+                            <p class="card-text"><strong>Choco Cokkies</strong></p>
+                            <p class="card-text">Rp 10.000</p>
+                            <hr>
+                            <p class="card-text">Pre Order - 1 h</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="btn-showmore  pt-5">
+                <a class="btn" href="/readmore">Show More</a>
+            </div>
+
+        </div>
+    </section>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,36 @@
+<footer class="footer text-center text-lg-start">
+    <div class="container pt-4">
+        <div class="row pt-5">
+            <div class="col-md-6">
+                <img style="width: 180px" src="img/Logo_Pre-Order.svg" alt="LogoPre_Order" width="50" class="me-2">
+                <br><br>
+                <hr>
+
+
+                <h2 class="fotter-tittle pt-5">Dukung Produk Buatan Mahasiswa Sanata Dharma</h2>
+                <h5 class="pt-4">Savor the dishes crafted by Sanata Dharma University students.</h5>
+
+                <div class="follow-us-container d-flex align-items-center pt-5">
+                    <h3 class="fotter-tittle">Follow us</h3>
+                    <img style="margin-left: 75px" width="70px" src="img/icon_instagram.svg" alt="Instagram"
+                        class="social-icon">
+                    <img style="margin-left: 100px" width="70px" src="img/icon_facebook.svg" alt="Facebook"
+                        class="social-icon">
+                    <img style="margin-left: 100px" width="70px" src="img/icon_twitter.svg" alt="Twitter"
+                        class="social-icon">
+                </div>
+                <div style="text-align:left" class="kendala-container">
+                    <h5>Kendala Pesanan? Silakan Hubungi Kami</h5>
+                    <p>Kirimkan pesan beserta bukti di wa kami</p>
+                    <button class="whatsapp-button">Whatsapp</button>
+                </div>
+                <div class="pt-5" >Karya anak Sanata Dharma. Pre-Order 2023</div>
+            </div>
+            <div class="col-md-6 d-flex justify-content-center align-items-center">
+                <img style="background-color: black" src="img/Footer-Picture.svg" class="img-fluid" alt="...">
+            </div>
+            
+
+        </div>
+    </div>
+</footer>
