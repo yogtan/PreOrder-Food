bc48dd2d1d127eb1b9e54dba732cea0dbbf50209
abdisavia <ridhodanabdi7a@gmail.com>
2023-10-06 11:29:35 +0700

    New Update

---
+++ resources/views/Admin/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('container')
+<nav class="navbar">
+    <div class="brand">
+        <h1><span>Pre -</span> order</h1>
+        <img src="img/icon_menu.svg" alt="icon menu">
+    </div>
+</nav>
+@endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,6 +15,7 @@
 
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/sejarah.css">
+    <link rel="stylesheet" href="../css/admin.css">
 </head>
 
 <body>
