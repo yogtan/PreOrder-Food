81e66145618ab4c15afa3f742e15d3002e961999
abdisavia <ridhodanabdi7a@gmail.com>
2023-11-03 13:33:15 +0700

    New Update Penjual Front End

--- resources/views/partials/NavbarPenjual.blade.php
+++ resources/views/partials/NavbarPenjual.blade.php
@@ -1,7 +1,7 @@
 
 <nav class="nav flex-column border w-25 h-100 position-fixed bg-white">
   <a class=" fs-2 fw-bold border-bottom  text-center py-5" href=""><img src="/img/Logo_Pre-Order.svg" alt="Logo PreOrder"></a>
-  <a class="nav-link fs-4 text-center d-flex items-center justify-content-center" href="/penjual/dashboard">
+  <a class="nav-link fs-4 text-center d-flex items-center justify-content-center" href="/penjual/">
     <img src="/img/icon_Monitor.svg" class="h-100" alt="" width=30 class="me-3"/>
     <h3 class="h-100 px-3">Dasboard</h3>
   </a>
--- resources/views/penjual/pesanan.blade.php
+++ resources/views/penjual/pesanan.blade.php
@@ -25,25 +25,42 @@ $menus = [];
                     <p class="py-2"><img src="/img/icon_arrow.svg" class="px-2"/>Jumlah produk yang tersedia</p>
                 </div>
                 <div>
-                    <table class="w-100 text-center">
-                        <thead class="bg-red">
+                    <table class="w-100 text-center t-Pesanan">
+                        <thead class="bg-red text-white">
                             <th>No</th>
                             <th>Nama Pembeli</th>
                             <th>Pesanan</th>
                             <th>Kuantitas</th>
                             <th>Total Harga</th>
-                            <th>Status</th>
+                            <th>Catatan</th>
                             <th>Tindakan</th>
                         </thead>
-                        <tbody>
-                            <tr>
+                        <tbody class="">
+                            <tr class="" >
                                 <td>1</td>
                                 <td>Jarwo</td>
-                                <td>Gado-Gado</td>
+                                <td class="">
+                                    <p class="text-break m-auto" style="width:100px;">Gado-gado</p>
+                                </td>
                                 <td>1</td>
                                 <td>Rp.10.000</td>
-                                <td>Proses</td>
-                                <td><button class="rounded-pill bg-red px-3 py-1 text-white">Selesai</button></td>
+                                <td class="">
+                                    <p class="text-break text-center d-block m-auto" style="width:100px;">Tidak Menggunakan Lontong</p>
+                                </td>
+                                <td><button class="rounded-pill bg-red px-3 py-1 text-white border-0">Selesai</button></td>
+                            </tr>
+                            <tr class="">
+                                <td>2</td>
+                                <td>Ami</td>
+                                <td class="">
+                                    <p style="width:100px;" class="text-break m-auto">Nasi Goreng Udang Sambel Matah</p>
+                                </td>
+                                <td>1</td>
+                                <td>Rp.10.000</td>
+                                <td class="">
+                                    <p style="width:100px;" class="text-break m-auto">Tanpa Menggunakan Nasi</p>
+                                </td>
+                                <td><button class="rounded-pill bg-red px-3 py-1 text-white border-0">Selesai</button></td>
                             </tr>
                         </tbody>
                     </table>
