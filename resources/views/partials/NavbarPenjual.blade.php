721d139b86b1efe0099c18b856f28369f0b94857
abdisavia <ridhodanabdi7a@gmail.com>
2023-12-17 16:33:25 +0700

    Reponsive at NavbarPenjual, headerPenjual, DashboardPenjual

--- resources/views/partials/NavbarPenjual.blade.php
+++ resources/views/partials/NavbarPenjual.blade.php
@@ -1,35 +1,154 @@
 
-<nav class="nav flex-column border w-25 h-100 position-fixed bg-white">
-  <a class=" fs-2 fw-bold border-bottom  text-center py-5" href=""><img src="/img/Logo_Pre-Order.svg" alt="Logo PreOrder"></a>
-  <a class="nav-link fs-1 text-center d-flex items-center justify-content-center" href="/penjual/">
-    <div class="grid">
-      <img src="/img/icon_Monitor.svg" class="h-100" alt="" width=40 class="me-3"/>
-      <h3 class="h-100 ms-3">Dasboard</h3>
-    </div>
+<nav class="bg-white border h-100 position-fixed p-xl-4 p-md-2 d-none d-md-block">
+  <a class="nav-link fs-2 fw-bold border-bottom  text-center my-xl-3 border" href="">
+    <img src="/img/Logo_Pre-Order.svg" alt="Logo PreOrder" class="border">
   </a>
-  <a class="nav-link fs-1 text-center d-flex items-center justify-content-center" href="/penjual/product">
-    <div class="grid">
-      <img src="/img/icon_kart.svg" class="h-100" alt="" width=40 class="me-3"/>
-      <h3 class="h-100 ms-3">Product</h3>
+  <div class="row border border-primary">
+    <div class="col border border-danger">
+      <a class="nav-link fs-1 fs-md-4 text-center" href="/penjual/">
+        <div class="row border border-primary align-items-center">
+          <div class="col-4 border">
+            <img src="/img/icon_Monitor.svg" class="" alt="" width=40 class=""/>
+          </div>
+          <div class="col-8 border">
+            <h3 class="">Dasboard</h3>
+          </div>
+        </div>
+      </a>
+      <a class="nav-link fs-1 text-center" href="/penjual/product">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-4 border">
+            <img src="/img/icon_kart.svg" alt="" width=40 />
+          </div>
+          <div class="col-8">
+            <h3 class="">Product</h3>
+          </div>
+        </div>
+      </a>
+      <a class="nav-link fs-1 text-center" href="/penjual/kelolaPesanan">
+        <div class="row align-items-center justify-content-center">
+          <div class="col-4 border">
+            <img src="/img/icon_pesanan.svg" alt="" width=40 />      
+          </div>
+          <div class="col-8 border">
+            <h3 class="">Pesanan</h3>
+          </div>
+        </div>
+      </a>
+      <a class="nav-link text-center " href="/penjual/laporanBulanan">
+        <div class="row align-items-center ">
+          <div class="col-4 border ">
+            <img src="/img/icon_money.svg" class="border" alt="" width=40 class="my-auto"/>
+          </div>
+          <div class="col-8 border">
+            <h3 class="border">Laporan Bulanan</h3>
+          </div>
+        </div>
+      </a>
+      <a class="nav-link text-center " href="/penjual/editPenjual/{{ auth()->user()->id }}">
+        <div class="row align-items-center">
+          <div class="col border align-items-center d-flex justify-content-center">
+            <img src="/img/edit.svg" alt="" width=40 class=""/>
+          </div>
+          <div class="col-8">
+            <h3 class="h-100">Edit Profile</h3>
+          </div>
+        </div>
+      </a>
     </div>
-  </a>
-  <a class="nav-link fs-1 text-center d-flex items-center justify-content-center" href="/penjual/kelolaPesanan">
-    <div class="grid ">
-      <img src="/img/icon_pesanan.svg" class="h-100 " alt="" width=40 class="me-3"/>
-      <h3 class="h-100  ms-3 ">Pesanan</h3>
-    </div>
-  </a>
-  <a class="nav-link fs-1 text-center d-flex items-center justify-content-center " href="/penjual/laporanBulanan">
-    <div class="grid">
-      <img src="/img/icon_money.svg" class="h-100 " alt="" width=40 class="me-3"/>
-      <h3 class="h-100 ms-3 ">Laporan Bulanan</h3>
-    </div>
-  </a>
-  <a class="nav-link fs-1 text-center d-flex items-center justify-content-center " href="/penjual/editPenjual/{{ auth()->user()->id }}">
-    <div class="grid">
-      <img src="/img/edit.svg" class="h-100 " alt="" width=40 class="me-3"/>
-      <h3 class="h-100 ms-3 ">Edit Profile</h3>
-    </div>
-  </a>
+  </div>
 </nav>
 
+
+<!-- for mobile -->
+<div class="offcanvas-lg offcanvas-end d-md-none" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
+  <div class="container">
+    <div class="offcanvas-header">
+      <img src="/img/icon_Profile.svg" alt="Img Profile" width="50" class="d-block">
+      <div class="btn-group text-secondary border ">
+            <h5 class=" d-block h-auto text-center my-auto" style="width:175px;">{{ auth()->user()->name }}</h5>
+              <!-- {{-- <button type="button" class="btn my-auto me-3 d-inline text-white">Welcome, {{ auth()->user()->name }}</button> --}} -->
+            <button type="button" class="btn dropdown-toggle dropdown-toggle-split h-100" data-bs-toggle="dropdown" aria-expanded="false">
+              <span class="visually-hidden">Toggle Dropdown</span>
+            </button>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="/penjual">home</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li>
+                <form action="/logout" method="post">
+                    @csrf
+                  <button type="submit" class="dropdown-item">Log Out</button>
+                </form>
+             </li>
+           </ul>
+        </div>      
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
+    </div>
+    <hr class="border-b-2">
+    <div class="offcanvas-body">
+      <ul class="navbar-nav">
+        <li class="">
+          <a href="/penjual/" class="text-decoration-none text-secondary">
+            <div class="row align-items-center">
+              <div class="col-2">
+                <img src="/img/icon_Monitor.svg" class="" alt="" width=40 class=""/>
+              </div>
+              <div class="col-6">
+                <h3 class="text-left">Dasboard</h3>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="mt-3">
+          <a href="/penjual/product" class="text-decoration-none text-secondary">
+            <div class="row align-items-center ">
+              <div class="col-2">
+                <img src="/img/icon_kart.svg" alt="" width=40 />
+              </div>
+              <div class="col-6">
+                <h3 class=" text-left">Product</h3>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li>
+        <li class="mt-3">
+          <a href="/penjual/kelolaPesanan" class="text-decoration-none text-secondary">
+            <div class="row align-items-center ">
+              <div class="col-2">
+                <img src="/img/icon_pesanan.svg" alt="" width=40 />      
+              </div>
+              <div class="col-8">
+                <h2 class=" text-left">Pesanan</h2>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="mt-3">
+          <a href="/penjual/laporanBulanan" class="text-decoration-none text-secondary">
+            <div class="row align-items-center ">
+              <div class="col-2">
+                <img src="/img/icon_money.svg" class="border" alt="" width=40 class="my-auto"/>
+              </div>
+              <div class="col-8">
+                <h2 class=" text-left">Laporan Bulanan</h2>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="mt-3">
+          <a href="/penjual/editPenjual/{{ auth()->user()->id }}" class="text-decoration-none text-secondary">
+            <div class="row align-items-center ">
+              <div class="col-2">
+                <img src="/img/edit.svg" alt="" width=40 class=""/>
+              </div>
+              <div class="col-6">
+                <h3 class="text-left">Edit Profile</h3>
+              </div>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
--- resources/views/penjual/dashboard.blade.php
+++ resources/views/penjual/dashboard.blade.php
@@ -1,92 +1,125 @@
 @extends('layouts.penjual')
 
 @section('penjualContent')
-<div class="content">
-    <div class="container px-5 mt-5">
-        <h1 class="text-white">Dashboard</h1>
-        @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
-
-        @if(session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-        @endif
-        <div class="cards mt-5 ">
-            <div class="card mx-auto p-3 shadow-1" style="width: 30rem; height:20rem;">
-                <div class="card-body">
-                    <div class="mb-6 card-content">
-                        <h6 class="fs-3">PRODUCT</h6>
-                        <h3 class=" text-success fs-1 fw-bold ">{{ $totalProducts }}</h3>
-                        <div class=" d-flex justify-content-center">
-                            <img src="/img/icon_house.svg" alt="" width="100" class="">
+<div class="content overflow-hidden pe-xl-3">
+    <div class="row">
+        <div class="col-md-5 col-lg-4 col-xl-3 col-xxl-2 border"></div>
+        <div class="col justify-content-center border">
+            <div class="px-2 mt-5 d-sm-block ">
+                <div class="row">
+                    <div class="col ">
+                        <h1 class="text-black">Dashboard</h1>
+                        @if(session('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
                         </div>
-                    </div>
-                    <div class="d-flex items-center">
-                        <img src="/img/icon_arrow.svg" alt="arrow" class="">
-                        <p class="ms-2 fs-4 my-auto">Jumlah Produk</p>
+                        @endif
+                
+                        @if(session('error'))
+                        <div class="alert alert-danger">
+                            {{ session('error') }}
+                        </div>
+                        @endif
                     </div>
                 </div>
-            </div>
-            <div class="card mx-auto p-3 shadow-1" style="width: 30rem; height:20rem;">
-                <div class="card-body">
-                    <div class="mb-6 card-content">
-                        <h6 class="fs-3">PESANAN</h6>
-                        <h3 class="text-success fs-1 fw-bold ">{{ $totalOrder }}</h3>
-                        <div class=" d-flex justify-content-center">
-                            <img src="/img/icon_house.svg" alt="" width="100" class="">
+                <div class="row gap-3 border">
+                    <div class="col col-sm-12 col-xl justify-content-end d-flex">
+                        <div class="card mx-auto p-3 shadow-1 h-100" >
+                            <div class="card-body">
+                                <div class=" card-content mb-5">
+                                    <div class="row">
+                                        <div class="col-xl-10 col-8">
+                                            <h6 class="fs-3">PRODUCT</h6>
+                                            <h3 class=" text-success fs-1 fw-bold ">{{ $totalProducts }}</h3>
+                                        </div>
+                                        <div class="col-4 col-xl-2 d-flex justify-content-end align-items-center">
+                                            <img src="/img/icon_kart_green.svg" alt=""  class="w-100">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="d-flex items-center ">
+                                    <img src="/img/icon_arrow.svg" alt="arrow" class="">
+                                    <p class="ms-2 fs-4 my-auto">Jumlah Produk</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="d-flex items-center">
-                        <img src="/img/icon_arrow.svg" alt="arrow" class="">
-                        <p class="ms-2 fs-4 my-auto">Jumlah pesanan baru hari ini</p>
+                    <div class="col col-xl col-sm-12">
+                        <div class="card mx-auto p-3 shadow-1 h-100">
+                            <div class="card-body">
+                                <div class=" card-content mb-5">
+                                    <div class="row align-items-center">
+                                        <div class="col-xl-10 col-8">
+                                            <h6 class="fs-3">PESANAN</h6>
+                                            <h3 class="text-success fs-1 fw-bold ">{{ $totalOrder }}</h3>
+                                        </div>
+                                        <div class="col-4 col-xl-2 d-flex justify-content-end align-items-center">
+                                            <img src="/img/icon_pesananGreen.svg" alt="" class="w-100">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="d-flex items-center">
+                                    <img src="/img/icon_arrow.svg" alt="arrow" class="">
+                                    <p class="ms-2 fs-4 my-auto">Jumlah pesanan baru hari ini</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div> 
+                </div>       
+                <div class="row">
+                    <div class="col">
+                        <div class="pendapatan p-4  shadow-1 rounded-1 h-auto ">
+                            <div class=" align-items-center row ">
+                                <div class=" col-xl-11 col-9" >
+                                    <h3>PENDAPATAN</h6>
+                                    <h1 class="text-success">Rp {{ number_format($totalEarn, 0, ',', '.') }}</h1>
+                                </div>
+                                <div class="col-3 col-xl-1 d-flex justify-content-end align-items-center">
+                                    <img src="/img/icon_money green.svg" alt="moneyicon" class="w-100">
+                                </div>
+                            </div>
+                            <div class="d-flex items-center mt-4 mb-5">
+                                <img src="/img/icon_arrow.svg" alt="arrow">
+                                <p class="fs-5 my-auto ms-2">Total Pendapatan</p>
+                            </div>
+                            <hr>
+                            <div class="row justify-content-center align-items-center">
+                                <div class="col-12 col-sm-12 overflow-x-auto ">
+                                    <!-- table for mobile -->
+                                    <table class="border h-auto table-w-default mx-auto">
+                                        <thead class="border text-center bg-hijau text-white fw-bold">
+                                            <th class=" py-sm-4 py-2 px-2">No</th>
+                                            <th class=" py-sm-4 py-2 px-2">Nama Pembeli</th>
+                                            <th class=" py-sm-4 py-2 px-2">Nama Produk</th>
+                                            <th class=" py-sm-4 py-2 px-2">Kuantitas</th>
+                                            <th class=" py-sm-4 py-2 px-2">Total Harga</th>
+                                        </thead>
+                                        <tbody class="text-center">
+                                        @foreach ( $orders as $order)
+                                            <tr class="">
+                                                <td class="py-4">{{ $loop->iteration }}</td>
+                                                <td class="py-4">{{ $order->name }}</td>
+                                                <td class="py-4">{{ $order->nama_produk }}</td>
+                                                <td class="py-4">{{ $order->total_produk }}</td>
+                                                <td class="py-4">Rp {{ number_format($order->harga_pembayaran, 0, ',', '.') }}</td>
+                                            </tr>
+                                        @endforeach
+                                       {{-- <tr class="">
+                                                <td class="py-4">1</td>
+                                                <td class="py-4">Abdi sang</td>
+                                                <td class="py-4">Risoles</td>
+                                                <td class="py-4">1</td>
+                                                <td class="py-4">Rp.10.000</td>
+                                            </tr> --}}
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                </div>
                 </div>
             </div>
         </div>
-
-        <div class="pendapatan container shadow-1 rounded-1 h-auto">
-            <div class="title d-flex justify-content-between items-center">
-                <h3>PENDAPATAN</h6>
-                <img src="/img/icon_money green.svg" alt="moneyicon" width="80">
-            </div>
-            <h1 class="text-success">Rp {{ number_format($totalEarn, 0, ',', '.') }}</h1>
-            <div class="d-flex items-center mt-4 mb-5">
-                <img src="/img/icon_arrow.svg" alt="arrow">
-                <p class="fs-5 my-auto ms-2">Total Pendapatan</p>
-            </div>
-            <hr>
-            <table class="border w-100 h-auto">
-                <thead class="border text-center bg-hijau text-white fw-bold">
-                    <th class=" py-4">No</th>
-                    <th class=" py-4">Nama Pembeli</th>
-                    <th class=" py-4">Nama Produk</th>
-                    <th class=" py-4">Kuantitas</th>
-                    <th class=" py-4">Total harga</th>
-                </thead>
-                <tbody class="text-center">
-                    @foreach ( $orders as $order)
-                    <tr class="">
-                        <td class="py-4">{{ $loop->iteration }}</td>
-                        <td class="py-4">{{ $order->name }}</td>
-                        <td class="py-4">{{ $order->nama_produk }}</td>
-                        <td class="py-4">{{ $order->total_produk }}</td>
-                        <td class="py-4">Rp {{ number_format($order->harga_pembayaran, 0, ',', '.') }}</td>
-                    </tr>
-                    @endforeach
-                    {{-- <tr class="">
-                        <td class="py-4">1</td>
-                        <td class="py-4">Abdi sang</td>
-                        <td class="py-4">Risoles</td>
-                        <td class="py-4">1</td>
-                        <td class="py-4">Rp.10.000</td>
-                    </tr> --}}
-                </tbody>
-            </table>
-        </div>
-    </div>
 </div>
 @endsection
